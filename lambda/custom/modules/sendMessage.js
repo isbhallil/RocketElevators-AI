cc854f49016078cb9d90f9b407b1c383d95df577
mo bhallil <is.bhallil@gmail.com>
2019-11-22 23:41:33 -0500

    .env credientials

--- lambda/custom/modules/sendMessage.js
+++ lambda/custom/modules/sendMessage.js
@@ -1,4 +1,4 @@
-const client = require('twilio')("AC221e628d23f16051361563539e040a4b", "e34f37f152128864a8d239e6817d25c3");
+const client = require('twilio')(process.env.twilioAccountSid, process.env.twilioAuthToken);
 
 module.exports = async (message, to) => {
     return await client.messages.create({
